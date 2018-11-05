bbf8e5edad017daac0e331e2e0e51e01fc3211dc
1995tadas <1995tadas@gmail.com>
2018-11-05 16:07:29 +0200

    2 užduotis Html ir php basics (FIX)

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Aš valdau HTML <?php print "ir PHP" ?></title>
-</head>
-<body>
-       
-   <h1><b>Tadas</b> - HTML <?php print "ir PHP"?> asas</h1>
-<p>Kuriu dinaminį turinį ir php
-atidaryta <?php 
-$data = date('d/m/Y h:i:s a');
-print $data;
-?>
-</p>  
-</body>
+<html lang="lt">
+    <head>
+        <meta charset="UTF-8">
+        <title>Aš valdau HTML <?php print "ir PHP" ?></title>
+    </head>
+    <body>
+        <h1><b>Tadas</b> - HTML <?php print "ir PHP" ?> asas</h1>
+        <p>
+            Kuriu dinaminį turinį ir php atidaryta 
+            <?php print date('d/m/Y h:i:s a'); ?>
+        </p>  
+    </body>
 </html>
 
