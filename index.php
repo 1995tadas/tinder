0959a3a6a0091071f9b9d38ac2525e3d8d1c9779
1995tadas <1995tadas@gmail.com>
2018-11-06 13:58:03 +0200

    PHP 6 užduotis div kvadratas random

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 <html lang="lt">
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
         <title>inline</title>
     </head>
-    <body style = "background-color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255) ?>)";>
-        <h1 style = "font-size:<?php print rand(5, 50) . 'px' ?>">Tekstas</h1>
-        <p style = "color:rgb(<?php print rand(0, 255) . ',' . rand(0, 255) . ',' . rand(0, 255) ?>)">D.Trump's nebebus prezidentu <?php print rand(2021, 2031) . ' ' . date('m d'); ?></p>
+    <body>
+        <div style= "<?php print "top:" . rand(0, 718) . "px;" . "left:" . rand(0, 1316) . "px;" ?>
+             ">
+        </div>
     </body>
 </html>
 
