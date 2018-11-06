10fe4182ec3a50de480ad98295898fd19348c8cf
1995tadas <1995tadas@gmail.com>
2018-11-06 11:07:09 +0200

    PHP 3 užduotis komentarai

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
-<?php
-date_default_timezone_set('Europe/Vilnius');
-?>
 <!DOCTYPE html>
 <html lang="lt">
     <head>
         <meta charset="UTF-8">
-        <title> PHP lydės ir <?php print 'ryt ' . date('d-m-Y', strtotime('+1 day')); ?></title>
+        <title>Komentarai </title>
         <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <h1><b>Tadas</b> - PHP su manim buvo ir <?php print date('G', strtotime('-1 hour')) . ' valanda!' ?></h1>
-        <p>
-            <?php print date("Y", strtotime("+1 year")) ?> ne už kalnų!
-        </p>  
+        <?php
+        /*
+          <div>
+          <h1>Aš pradedu</h1>
+          <h2>O aš papildau</h2>
+          </div>
+          <!-- Štai čia yra pirmasis div konteineris kuriame yra du h tagai -->
+         */
+        ?>
+        <!-- 
+        <div>
+             <p>
+             Aš esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba...
+             </p> 
+         </div>!
+        -->
     </body>
 </html>
 
