7bf1acc4951aa088f40bee64fbef52c8e690c510
1995tadas <1995tadas@gmail.com>
2018-11-06 10:31:21 +0200

    PHP 2 užduotis date () & strtime()

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
+<?php
+date_default_timezone_set('Europe/Vilnius');
+?>
 <!DOCTYPE html>
 <html lang="lt">
     <head>
         <meta charset="UTF-8">
-        <title><?php print 'Aš, '. date('l').' ir php'; ?></title>
+        <title> PHP lydės ir <?php print 'ryt ' . date('d-m-Y', strtotime('+1 day')); ?></title>
         <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <h1><b>Tadas</b> - HTML <?php print "ir PHP" ?> asas jau <?php print date('Y').' metų';?></h1>
+        <h1><b>Tadas</b> - PHP su manim buvo ir <?php print date('G', strtotime('-1 hour')) . ' valanda!' ?></h1>
         <p>
-            Viskas prasidėjo 
-            <?php print date('m').' mėnesio '.date('d').' dieną !';?>
+            <?php print date("Y", strtotime("+1 year")) ?> ne už kalnų!
         </p>  
     </body>
 </html>
