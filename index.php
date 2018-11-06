8ffd124d65ba7db061ea3ae94e55b082fd70354c
1995tadas <1995tadas@gmail.com>
2018-11-06 09:37:38 +0200

    PHP 1 užduotis STRING

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html lang="lt">
     <head>
         <meta charset="UTF-8">
-        <title>Aš valdau HTML <?php print "ir PHP" ?></title>
+        <title><?php print 'Aš, '. date('l').' ir php'; ?></title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
     </head>
     <body>
-        <h1><b>Tadas</b> - HTML <?php print "ir PHP" ?> asas</h1>
+        <h1><b>Tadas</b> - HTML <?php print "ir PHP" ?> asas jau <?php print date('Y').' metų';?></h1>
         <p>
-            Kuriu dinaminį turinį ir php atidaryta 
-            <?php print date('d/m/Y h:i:s a'); ?>
+            Viskas prasidėjo 
+            <?php print date('m d').' dieną !';?>
         </p>  
     </body>
 </html>
