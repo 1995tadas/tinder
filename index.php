e5024097de072f18f0fce7f158690c9bc521daf5
1995tadas <1995tadas@gmail.com>
2018-11-07 12:46:59 +0200

    PHP uzduotis skaiciavimas atlyginimo

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-$img = '/images/explosion.gif';
-$link = 'https://www.google.com';
+$mano_pinigai = 1000;
+$spent_per_month = 600;
+$earned_per_month = 800;
+$unknown_per_month = rand(-150, 150);
+$months = 24;
+$data = date('Y', strtotime("+$months months"));
+$wallet_forecast = $mano_pinigai + ($earned_per_month - $spent_per_month + $unknown_per_month) * $months;
 ?>
 
 <!DOCTYPE html>
@@ -9,14 +14,6 @@ $link = 'https://www.google.com';
         <title></title>
     </head>
     <body>
-        <img src=<?php print $img; ?>>
-        <img src=<?php print $img; ?>>
-        <img src=<?php print $img; ?>>
-        <img src=<?php print $img; ?>>
-
-        <a href=<?php $link ?>>Linkas</a>
-        <a href=<?php $link ?>>Linkas</a>
-        <a href=<?php $link ?>>Linkas</a>
-        <a href=<?php $link ?>>Linkas</a>
+        <p> Po <?php print $months . ' (' . $data . ')' ?> mėnesių turėsiu: <?php print $wallet_forecast ?> JEVRŲ!!!</p>
     </body>
 </html>
