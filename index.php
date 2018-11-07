98b5292bffb723961bfc9183dce283cdbc5aa998
1995tadas <1995tadas@gmail.com>
2018-11-07 10:27:20 +0200

    PHP 12 str_replace

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head> 
-        <meta http-equiv="refresh" content=1;URL='index.php'>
         <style>
-            .bomb{
-                transform:scale(0.<?php print date('s'); ?>); 
-                content:url("/images/Paveikslėlis1.png");
-            }
-            .bomb00{
-                transform:scale(1);
-                content:url("/images/explosion.gif");
-            }
         </style>
     </head>
     <body>
-        <div class="bomb<?php print date('s') ?> bomb" ></div>
         <?php
-        print date('s');
+        print str_replace(' ','<br>','I\'m addicted to brake fluid, but I can stop whenever I want.')
         ?>
     </body>
 </html>
