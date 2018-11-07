c4664ada4e044d8a553a6e21b1f6dbaca97b0db0
1995tadas <1995tadas@gmail.com>
2018-11-07 12:07:45 +0200

    PHP 2 variables img a

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
+<?php
+$img = '/images/explosion.gif';
+$link = 'https://www.google.com';
+?>
+
 <!DOCTYPE html>
 <html>
     <head> 
-        <style>
-        </style>
+        <title></title>
     </head>
     <body>
-        <?php
-        $string = "Tadas";
-        $integer = 0;
-        $float = 2.145;
-        $bolean = true;
-        $null = null;
-        print "$string <br> $integer <br> $float <br> $bolean <br> $null";
-        ?>
+        <img src=<?php print $img; ?>>
+        <img src=<?php print $img; ?>>
+        <img src=<?php print $img; ?>>
+        <img src=<?php print $img; ?>>
+
+        <a href=<?php $link ?>>Linkas</a>
+        <a href=<?php $link ?>>Linkas</a>
+        <a href=<?php $link ?>>Linkas</a>
+        <a href=<?php $link ?>>Linkas</a>
     </body>
 </html>
