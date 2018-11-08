9c72bb2c5dc1de41c15f0ad9264cc52cce0ec87e
1995tadas <1995tadas@gmail.com>
2018-11-08 09:02:26 +0200

    PHP round kuras

--- index.php
+++ index.php
@@ -1,48 +1,22 @@
 <?php
-$sec = date('s');
-if ($sec % 2 == 0) {
-    $klase = "square";
-} else {
-    $klase = "circle";
-}
+$distance = rand(500, 1000);
+$fuel_100km = 7.5;
+$fuel_price = 1.3;
+$fuel = ($distance / 100) * $fuel_100km;
+$price = $fuel * $fuel_price;
+$text = "Nuvažiavus $distance  km, mašina sunaudos " . round($fuel) . " l. kuro.
+Kaina " . round($price) . " Jevrų !!!";
 ?>
 <!DOCTYPE html>
 <html>
     <head> 
-        <meta http-equiv="refresh" content=1;URL='index.php'">
         <title></title>
         <style>
-            .centras{
-                text-align: center;
-                position: absolute;
-                left: 50px;
-                top: -165px;
-                font-size:300px;
-            }
-            .vidurys {
-                position: absolute;
-                margin: auto;
-                top: 0;
-                right: 0;
-                bottom: 0;
-                left: 0;
-            }
-            .square {
-                width: 400px;
-                height: 400px;
-                background: red;
-            }
-            .circle {
-                width: 400px;
-                height: 400px;
-                background: red;
-                border-radius: 50%;
-            }
         </style>
     </head>
     <body>
-        <div class="vidurys <?php print "$klase" ?>">
-            <h1 class="centras"><?php print "$sec" ?></h1>
-        </div>
+        <h1>
+            <?php print "$text" ?>
+        </h1>
     </body>
 </html>
