c1045e2548c5a840b4d013328f3ef7394a3e5abe
1995tadas <1995tadas@gmail.com>
2018-11-08 12:35:35 +0200

    PHP for ciklas

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
-$distance = rand(500, 1000);
-$fuel_100km = 7.5;
-$fuel_price = 1.3;
-$my_money = 70;
-$bako_talpa = 50;
-$fuel = ($distance / 100) * $fuel_100km;
-$price = $fuel * $fuel_price;
-$head = "Nuvažiavus $distance  km, mašina sunaudos " . round($fuel) . " l. kuro.
-Kaina " . round($price) . " Jevrų !!!<br>";
-
-if ($my_money >= $price) {
-    $head .= 'galiu <br>';
-} else {
-    $head .= 'negaliu <br>';
-}
-
-if($bako_talpa < $fuel ) {
-    $head .= "Kuro pakartotinai pilti reikės ";
-    $pilimas = $bako_talpa/$fuel;
-    $head .= ceil($pilimas)." kartą";
-} else {
-    $head .= "Kuro pakartotinai pilti nereikės";
+$text = 'Pradedam:';
+for ($x = 0; $x <= 10; $x++) {
+    $text .= ' Ciklas';
 }
 ?>
 <!DOCTYPE html>
@@ -28,11 +9,11 @@ if($bako_talpa < $fuel ) {
     <head> 
         <title></title>
         <style>
+
         </style>
     </head>
-    <body>
-        <h1>
-            <?php print $head ?>
+    <body >
+        <h1><?php print $text ?>
         </h1>
     </body>
 </html>
