bc6e5754aa7d5979851765b32c0860bb8d40bd79
1995tadas <1995tadas@gmail.com>
2018-11-08 10:22:49 +0200

    PHP kuras skaiciavimai

--- index.php
+++ index.php
@@ -2,10 +2,26 @@
 $distance = rand(500, 1000);
 $fuel_100km = 7.5;
 $fuel_price = 1.3;
+$my_money = 70;
+$bako_talpa = 50;
 $fuel = ($distance / 100) * $fuel_100km;
 $price = $fuel * $fuel_price;
-$text = "Nuvažiavus $distance  km, mašina sunaudos " . round($fuel) . " l. kuro.
-Kaina " . round($price) . " Jevrų !!!";
+$head = "Nuvažiavus $distance  km, mašina sunaudos " . round($fuel) . " l. kuro.
+Kaina " . round($price) . " Jevrų !!!<br>";
+
+if ($my_money >= $price) {
+    $head .= 'galiu <br>';
+} else {
+    $head .= 'negaliu <br>';
+}
+
+if($bako_talpa < $fuel ) {
+    $head .= "Kuro pakartotinai pilti reikės ";
+    $pilimas = $bako_talpa/$fuel;
+    $head .= ceil($pilimas)." kartą";
+} else {
+    $head .= "Kuro pakartotinai pilti nereikės";
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,7 +32,7 @@ Kaina " . round($price) . " Jevrų !!!";
     </head>
     <body>
         <h1>
-            <?php print "$text" ?>
+            <?php print $head ?>
         </h1>
     </body>
 </html>
