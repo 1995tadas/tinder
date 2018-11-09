0db3433f2c3a8bd41ee3fb399b3b9b4f06d18788
1995tadas <1995tadas@gmail.com>
2018-11-09 15:09:14 +0200

    PHP array saldytuvas

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 $index = null;
+$sarasas = null;
+$PoVisko = null;
+$produktai = null;
 $fridge = ['Jogurtas', 'Kebabas', 'Alus', 'Sugedę vaisiai', 'Supuvęs avokadas'];
-for ($i = 0; $i < count($fridge); $i++) {
-    $index .= $fridge[$i]."<br>";
+$noriu = ['Kebabas', 'Alus', 'Pica'];
+
+foreach ($fridge as $value) {
+    $index .= $value . '<br>';
+}
+$random_idx = rand(0, count($fridge) - 1);
+$print = $fridge[$random_idx];
+
+foreach ($noriu as $norai) {
+    if (in_array($norai, $fridge) == 1) {
+        $produktai .= $norai . ' Turiu' . '<br>';
+    } else {
+        $produktai .= $norai . ' Neturiu' . '<br>';
+    }
+}
+
+$skirtumas = array_diff($fridge, $noriu);
+
+foreach ($skirtumas as $value) {
+    $PoVisko .= $value . '<br>';
 }
 ?>
 <!DOCTYPE html>
@@ -17,6 +38,20 @@ for ($i = 0; $i < count($fridge); $i++) {
         <p>
             <?php print $index ?>
         </p>
+        <h1>Šiandien valgysiu: </h1>
+        <p>
+            <?php print $print ?>
+        </p>
+
+        <h1>Ar viską turiu šaldytuve </h1>
+        <p>
+            <?php
+            print $produktai;
+            ?>
+        </p>
+
+        <h1>Šaldytuvo turinys po visko:</h1>
+        <p><?php print $PoVisko ?></p>
     </body>
 </body>
 </html>
