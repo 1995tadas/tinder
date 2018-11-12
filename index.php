df9d0e88d1c3315ee79ccf20909cf99fef8aedcc
1995tadas <1995tadas@gmail.com>
2018-11-12 15:39:14 +0200

    PHP Moters_rankinukas nebaigtas

--- index.php
+++ index.php
@@ -1,28 +1,22 @@
 <?php
-$produktas = null;
-$pavadinimas = null;
-$kaina = null;
-$aprasymas = null;
-$nuolaida = null;
-$catalog = [
-    "Bulve" => [
-        "pavadinimas" => "bulve",
-        "kaina" => 0.20,
-        "aprasymas" => "Populiariausia daržovė",
-    ],
-    "Kopustas" => [
-        "pavadinimas" => "kopustas",
-        "kaina" => 0.70,
-        "aprasymas" => "Lapai",
-        "nuolaida" => "5%"
-    ],
-    "Svogunas" => [
-        "pavadinimas" => "svogunas",
-        "kaina" => 0.50,
-        "aprasymas" => "Verksi",
-        "nuolaida" => "95%"
-    ]
-];
+$daiktu_pavadinimai = ["Kremas", "Riešutai", "Telefonas", "Agurkas", "Dažai", "Raktai", "Alus", "Pienas", "Burokas", "Bulve"];
+$head = null;
+$moters_rankinukas = [];
+for ($i = 0; $i <= rand(0, 5); $i++) {
+    $daiktas = rand(0, (count($daiktu_pavadinimai) - 1));
+    $size = rand(10, 20);
+
+    if (rand(0, 1)) {
+        $bright_dark = "Tamsus";
+    } else {
+        $bright_dark = "Sviesus";
+    }
+    $moters_rankinukas[] = [
+        "pavadinimas" => $daiktu_pavadinimai[$daiktas],
+        "dydis" => $size,
+        "spalva" => $bright_dark
+    ];
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -32,15 +26,8 @@ $catalog = [
         </style>
     </head>
     <body>
-        <?php foreach ($catalog as $item): ?>
-            <div class="produktas">
-                <span class="pavadinimas">Pavadinimas: <?php print $item['pavadinimas'] ?></span><br>
-                <span class="kaina">Kaina: <?php print $item['kaina'] ?></span><br>
-                <span class="aprasymas">Aprasymas: <?php print $item['aprasymas'] ?></span><br>
-                <?php if (isset($item['nuolaida'])): ?>
-                    <span class="nuolaida">Nuolaida: <?php print $item['nuolaida'] ?></span>
-                <?php endif; ?>
-            </div>
+        <?php foreach ($moters_rankinukas as $turinys): ?>
+            <h1><?php print $turinys["pavadinimas"] . " uzima " . $turinys['dydis'] . ' cm3. Daiktas yra ' . $turinys['spalva'] . ". Tikimybe rast yra :" ?></h1>
         <?php endforeach; ?>
     </body>
 </body>
