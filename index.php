b29fb78db09846d0318c7bc41b99def65c474ef0
1995tadas <1995tadas@gmail.com>
2018-11-13 12:59:56 +0200

    PHP 1 leap_year comments

--- index.php
+++ index.php
@@ -1,36 +1,38 @@
 <?php
+$black_ball = [
+    '100%' => "Visiškai taip!",
+    '70%' => "Taip",
+    '50%' => "Galbūt",
+    '30%' => "Ne",
+    '100%' => "Nė negalvok!"];
 
-/**
- * Funkcija surasti keliamiesiems metams
- * 
- * @param integer $x nurodytu metu parametras
- * @return string 
- */
-function is_leap_year($x) {
-
-
-    if (date('L', strtotime("$x-01-01")) == 1) {
-    //date('L') jeigu metai keliamieji grazina 1, jeigu ne 0
-    //srttotime() nurodyta teksttini laika pavercia date
-    //if() tikrina ar date('L') nurodyti metai  yra true or false jeigu true isveda,
-    // kad metai keliamieji jei false, kad nekeliamieji
-        return $x . ' metai yra keliamieji';
-    } else {
-        return $x . ' metai yra nekeliamieji';
-    }
+function black_ball($black_ball) {
+    return $black_ball[array_rand($black_ball)];
 }
-
-is
 ?>
 <!DOCTYPE html>
 <html>
     <head> 
         <title></title>
         <style>
+            body {
+                background-image:url("http://legomenon.com/images/magic8ball-new2.png");
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-position: center; 
+                position: relative;
+            }
+            h1 {
+                margin: 0;
+                position: absolute;
+                top: 300px;
+                left: 44%;
+                color:white;
+            }
         </style>
     </head>
     <body>
-        <h1> <?php print is_leap_year(2015) ?></h1>
+        <h1> <?php print black_ball($black_ball) ?></h1>
     </body>
 </body>
 </html>
