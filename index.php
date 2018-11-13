b3cb64a390c5706f577496ea411885c60c0b02f2
1995tadas <1995tadas@gmail.com>
2018-11-13 11:10:52 +0200

    PHP 1 leap_year comments

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
 
-function add($x, $y) {
-    $suma = $x + $y;
-    return "$x + $y suma:".$suma;
+/**
+ * Funkcija surasti keliamiesiems metams
+ * 
+ * @param integer $x nurodytu metu parametras
+ * @return string 
+ */
+function is_leap_year($x) {
+
+
+    if (date('L', strtotime("$x-01-01")) == 1) {
+    //date('L') jeigu metai keliamieji grazina 1, jeigu ne 0
+    //srttotime() nurodyta teksttini laika pavercia date
+    //if() tikrina ar date('L') nurodyti metai  yra true or false jeigu true isveda,
+    // kad metai keliamieji jei false, kad nekeliamieji
+        return $x . ' metai yra keliamieji';
+    } else {
+        return $x . ' metai yra nekeliamieji';
+    }
 }
+
+is
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,7 +30,7 @@ function add($x, $y) {
         </style>
     </head>
     <body>
-        <h1><?php print add(7, 8) ?></h1>
+        <h1> <?php print is_leap_year(2015) ?></h1>
     </body>
 </body>
 </html>
