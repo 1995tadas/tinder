8955176a27e64cfb3d87601a86c1fe40de567d4e
1995tadas <1995tadas@gmail.com>
2018-11-13 13:46:31 +0200

    PHP uzduotis 4 array while for

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <?php
-$black_ball = [
-    '100%' => "Visiškai taip!",
-    '70%' => "Taip",
-    '50%' => "Galbūt",
-    '30%' => "Ne",
-    '100%' => "Nė negalvok!"];
+$zodziai = [
+    "Petras",
+    "Ejo",
+    "Papjove",
+    "Arkli",
+    "Isgeres",
+    "Alu"
+];
+$zodziai2 = [
+    "condign",
+    "xylology",
+    "isohalsine",
+    "apophasis",
+    "koine",
+    "nictate"
+];
 
-function black_ball($black_ball) {
-    return $black_ball[array_rand($black_ball)];
+function demo_text($array, $lenght) {
+    $text = null;
+   for (;strlen($text) < $lenght;) {
+        $text .=$array[array_rand($array)] . " ";
+    }
+    return $text;
 }
 ?>
 <!DOCTYPE html>
@@ -15,24 +29,11 @@ function black_ball($black_ball) {
     <head> 
         <title></title>
         <style>
-            body {
-                background-image:url("http://legomenon.com/images/magic8ball-new2.png");
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-position: center; 
-                position: relative;
-            }
-            h1 {
-                margin: 0;
-                position: absolute;
-                top: 300px;
-                left: 44%;
-                color:white;
-            }
+
         </style>
     </head>
     <body>
-        <h1> <?php print black_ball($black_ball) ?></h1>
+        <h1> <?php print demo_text($zodziai, 10000) ?></h1>
     </body>
 </body>
 </html>
