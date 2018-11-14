1c5768ed7a67939833632d0da0bf04afe4d331df
1995tadas <1995tadas@gmail.com>
2018-11-14 14:07:14 +0200

    PHP POST form isset kvadrato suradimas

--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <?php
-$drinks_array = [
-    [
-        'pavadinimas' => 'Vodka',
-        'turis' => 0.7,
-        'promiles' => 40
-    ],
-    [
-        'pavadinimas' => 'Alus',
-        'turis' => 0.5,
-        'promiles' => 4.5
-    ]
-];
-
-/**
- * Programa kuri apskaičiuoja kiek bonkių\bambalių galima išgerti kol gryno alkoholio kiekis neviršis $max_level
- * 
- * @param type array $drinks_array
- * @param type integer $max_level
- * @return type array $drinks
- * */
-function drinks($drinks_array, $max_level) {
-    foreach ($drinks_array as $key => $drink) {
-        $pure_alcohol = ($drink['promiles'] / 100) * $drink['turis'];
-        //Apskaičiuojam vieno butelio gryną alkoholio kiekį
-        $drink['gali_isgert'] = floor($max_level / $pure_alcohol);
-        //alkoholio limitą padalinam iš gryno alkoholio kiekio ir gaunam bonkų\bambalių kiekį
-        $drinks_array[$key] = $drink;
-        //assigninam pakeistą vidinį masivą prie išorinio
-    };
-    return $drinks_array;
-    //gražinam į funkciją
+if (isset($_POST['tekstas'])) {
+    $skaicius = $_POST['tekstas'];
+    if (strlen($skaicius) > 0) {
+        $kvadratas = pow($skaicius, 2);
+    } else {
+        $kvadratas = "Input negali buti tuscias!!!";
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -39,6 +15,13 @@ function drinks($drinks_array, $max_level) {
         </style>
     </head>
     <body>
-        <h1> <?php var_dump(drinks($drinks_array, 5)) ?></h1>
+        <form action = "index.php" method ="POST">
+            Ka pakelti kvadratu:
+            <input name="tekstas" type="text"/>
+            <input type="submit"/>
+        </form>
+        <?php if (isset($kvadratas)): ?>
+            <h1><?php print $kvadratas ?></h1>
+        <?php endif; ?>
     </body>
 </html>
