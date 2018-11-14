3856ca11b985b90cdc17e368be4fa8bbb7fd2ad3
1995tadas <1995tadas@gmail.com>
2018-11-14 10:04:09 +0200

    PHP function array in array random  RAM ASSIGN IMPORTANT !!!

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
 <?php
-$zodziai = [
-    "Petras",
-    "Ejo",
-    "Papjove",
-    "Arkli",
-    "Isgeres",
-    "Alu"
+$cars_array = [
+    [
+        "modelis" => "Fiat Multipla",
+        "kaina" => "300"
+    ],
+    [
+        "modelis" => "Ford Ka",
+        "kaina" => "250"
+    ]
 ];
-$zodziai2 = [
-    "condign",
-    "xylology",
-    "isohalsine",
-    "apophasis",
-    "koine",
-    "nictate"
-];
-
-function demo_text($array, $lenght) {
-    $text = null;
-   for (;strlen($text) < $lenght;) {
-        $text .=$array[array_rand($array)] . " ";
-    }
-    return $text;
+/**
+ * Prideda prie esamo masyvo mašinos pardavimo kaina su nuolaida ir išspausdina ar apsimokėjo
+ * 
+ * @param array $cars_array
+ * @return string
+ */
+function sell_cars($cars_array) {
+    foreach ($cars_array as $key => $masina) {
+        $masina["pardavimo_kaina"] = rand($masina['kaina'] * 0.7, $masina['kaina'] * 1.3);
+        //$masina masyvo key pardavimo_kaina prideda rand +/- 30%
+        if ($masina["pardavimo_kaina"] > $masina['kaina']) {
+            $masina["apsimokejo"] = "taip";
+        } else {
+            $masina["apsimokejo"] = "ne";
+        }
+        //if tikrina ar pardavimo kaina yra didesne uz pradine kaina ir sukuria nauja key apsimokejo su reikšmemis taip arba ne
+        $cars_array[$key] = $masina;
+        //is ramu nuomenys perkelia į masyvą;
+    };
+    return $cars_array;
 }
+se
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,7 +41,7 @@ function demo_text($array, $lenght) {
         </style>
     </head>
     <body>
-        <h1> <?php print demo_text($zodziai, 10000) ?></h1>
+        <h1> <?php var_dump(sell_cars($cars_array)) ?></h1>
     </body>
 </body>
 </html>
