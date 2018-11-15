0bdb40339f4e9cb5ae74fef82758828083257ff2
1995tadas <1995tadas@gmail.com>
2018-11-15 11:34:20 +0200

    PHP cookie !IMPORTANT not wine with comments

--- index.php
+++ index.php
@@ -1,34 +1,72 @@
 <?php
-$answers_array = [
-    "TAIP",
-    "NE",
-    "I DON'T GIVE A FUCK"
-];
+/**
+ * Funkcija, kuri tiems patiems klausimas iš to pačio PC visada duotų tuos pačius atsakymus.
+ * Atsakymai su klausimais yra dedami į masyva ir overwritinami.
+ */
 
-function write_cookie($question_array) {
-    setcookie('magic_ball', json_encode($question_array), time() + 3600);
+$klausimas = null;
+$atsakymu_masyvas = ['Taip', 'Ne', 'Galbut'];
+
+/**
+ * Funkcija atsitiktinai isrenkanti viena atsakyma is masyvo.
+ * 
+ * @param type array $atsakymu_masyvas Masyvas su 3 atsakymais.
+ * @return type array $atsakymu_masyvas Grazina viena atsakyma is masyvo nuradant jo indeksa
+ */
+function get_random_answer($atsakymu_masyvas) {
+    $indexas = array_rand($atsakymu_masyvas, 1);
+    return $atsakymu_masyvas[$indexas];
 }
 
-function read_cookie($cookie_name) {
+/**
+ * Funkcija skirta patikrinti ar yra sukurtas cookie
+ * ir grazinti jame esancia decodinta data
+ * 
+ * @param integer $cookie_name Cookie pavadinimas
+ * @return array Data is Cookie (tuscias jei cookie negzistuoja)
+ */
+function cookie_read($cookie_name) {
     if (isset($_COOKIE[$cookie_name])) {
+        // Returninam decodinta cookie verte
         return json_decode($_COOKIE[$cookie_name], true);
     }
-    
+    // Jei nera sukurtas toks cookie (neegzistuoja), grazinam tuscia masyva
     return [];
 }
 
-$answer = $answers_array[array_rand($answers_array)];
+/**
+ * Funkcija, kuri sukuria nauja cookie
+ * 
+ * @param array integer $cookie_name Cookie pavadinimas
+ * @param array $data Kazkokia data
+ */
+function cookie_write($cookie_name, $data) {
+    setcookie($cookie_name, json_encode($data), time() + 3600);
+}
+
+//Tikriname ar buvo kazkas ivykdyta (submit)
 if (isset($_POST['klausimas'])) {
-    if (strlen($_POST['klausimas']) > 0) {
-        $question = $_POST['klausimas'];
-        if (!isset($question_array[$question])) {
-            $question_array[$question] = $answer;
-            $answer = $question_array[$question];
+// Jei buvo, prilyginam kintamajam
+    $klausimas = $_POST['klausimas'];
+
+    // Tikriname ar i forma buvo irasytas klausimas
+    if (strlen($klausimas) > 0) {
+        // Jei buvo metam ji i history
+        $answer_history = cookie_read('history');
+        var_dump($answer_history);
+        // Tikriname ar buvo setintas klausimas
+        if (isset($answer_history[$klausimas])) {
+            // Jei buvo, priskiriam ji senam atsakymui
+            // (Atsakymui, kuris jau buvo sugeneruotas atsitiktinai ir priskirtas klausimui)
+            $atsakymas = $answer_history[$klausimas];
+        } else {
+            // Iskvieciam get_random_answer funkcija, kuri sugeneruoja random atsakyma
+            $atsakymas = get_random_answer($atsakymu_masyvas);
+            // Klausima priskiriam atsakymui
+            $answer_history[$klausimas] = $atsakymas;
+            // Irasom nauja klausima su nauju atsakymu i cookie history
+            cookie_write('history', $answer_history);
         }
-        read_cookie($question_array);
-        $answer = $question_array[$question];
-        write_cookie($question_array);
-        var_dump($question_array);
     }
 }
 ?>
@@ -39,15 +77,13 @@ if (isset($_POST['klausimas'])) {
         </style>
     </head>
     <body>
-        <?php if (isset($question)): ?>
-            <h1><?php print $question ?></h1>
-            <h2><?php print $answer ?></h2>
-<?php endif; ?>
-
-        <form action = "index.php" method ="POST">
-            Klausimas:
-            <input name="klausimas" type="text" placeholder="Įvesk savo klausimą ..."/>
-            <input type="submit" value="Klausk !!!"/>
+        <form action="index.php" method="POST">
+            <b> Vardas: </b> <input name="klausimas" type="text"/>
+            <input type="submit"/>
         </form>
+        <?php if (isset($atsakymas)): ?>
+            <h1> Jūsų klausimas: <?php print $klausimas ?></h1>
+            <h2> Atsakymas: <?php print $atsakymas ?></h2>
+        <?php endif; ?>
     </body>
 </html>
