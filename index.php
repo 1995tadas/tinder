4844b9096051b71a5d88f6ab19bef4f0c14130f8
1995tadas <1995tadas@gmail.com>
2018-11-15 11:32:34 +0200

    PHP cookie not working mine

--- index.php
+++ index.php
@@ -4,11 +4,31 @@ $answers_array = [
     "NE",
     "I DON'T GIVE A FUCK"
 ];
-$answer = $answers_array[array_rand($answers_array)];
 
+function write_cookie($question_array) {
+    setcookie('magic_ball', json_encode($question_array), time() + 3600);
+}
+
+function read_cookie($cookie_name) {
+    if (isset($_COOKIE[$cookie_name])) {
+        return json_decode($_COOKIE[$cookie_name], true);
+    }
+    
+    return [];
+}
+
+$answer = $answers_array[array_rand($answers_array)];
 if (isset($_POST['klausimas'])) {
     if (strlen($_POST['klausimas']) > 0) {
         $question = $_POST['klausimas'];
+        if (!isset($question_array[$question])) {
+            $question_array[$question] = $answer;
+            $answer = $question_array[$question];
+        }
+        read_cookie($question_array);
+        $answer = $question_array[$question];
+        write_cookie($question_array);
+        var_dump($question_array);
     }
 }
 ?>
@@ -22,7 +42,7 @@ if (isset($_POST['klausimas'])) {
         <?php if (isset($question)): ?>
             <h1><?php print $question ?></h1>
             <h2><?php print $answer ?></h2>
-        <?php endif; ?>
+<?php endif; ?>
 
         <form action = "index.php" method ="POST">
             Klausimas:
