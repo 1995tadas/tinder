628dca9aa406618dbdbcd09ebde2ebae9c4715a8
1995tadas <1995tadas@gmail.com>
2018-11-19 09:40:55 +0200

    PHP oop class print string

--- index.php
+++ index.php
@@ -1,101 +1,29 @@
 <?php
-// Load all functions here
-require_once 'functions/core.php';
 
-// Enable/Disable Debugging
-$debug = false;
+class Text {
 
-/**
- * Default $page array:
- * Array of all page variables
- * and settings
- */
-$page = [
-    'title' => 'Error: 404',
-    'stylesheet' => 'main.css',
-    'content' => [
-// Šitas variable 'rendered' bus sukurtas controllerio
-// Tai bus ilgas HTML'o stringas sukurtas funkcijos
-// render_page()
-        'rendered' => 'Tokio puslapio nerasta'
-    ],
-    'header_links' => [
-        [
-            'url' => 'http://tadas.lt/home',
-            'id' => 'Home',
-            'text' => "HOME"
-        ],
-        [
-            'url' => 'http://tadas.lt/showcase',
-            'id' => 'Showcase',
-            'text' => "SHOWCASE"
-        ],
-               [
-            'url' => 'http://tadas.lt/matrica',
-            'id' => 'Showcase',
-            'text' => "MATRICA"
-        ]
-    ],
-    'show_header' => true,
-    'show_footer' => true,
-];
+    public $textas;
 
-/**
- * Čia yra mūsų "Router'is"
- * Kiekvienam page iškviečiame
- * atitinkamą controller'į
- */
-if (isset($_GET['page'])) {
-    $page_name = $_GET['page'];
-
-    switch ($page_name) {
-        case 'home':
-            run_controller($page, 'home');
-            break;
-        case 'cv':
-            run_controller($page, 'cv');
-            break;
-        case 'showcase':
-            run_controller($page, 'showcase');
-            break;
-         case 'matrica':
-            run_controller($page, 'matrica');
-            break;
-        default:
-        // run_controller($page, '404');
+    public function __construct($textas) {
+        $this->textas = $textas;
     }
-} else {
-    run_controller($page, 'home');
+
+
 }
+
+$spausdink = new Text('„Kam to reikia!?
+ Man patinka funkcijos”');
+print $spausdink->textas;
 ?>
-<html>
+
+
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <title><?php print $page['title']; ?></title>
-        <link rel="stylesheet" type="text/css" href="/css/<?php print $page['stylesheet']; ?>">
+        <meta charset="UTF-8">
+        <title>Document</title>
     </head>
     <body>
-        <!-- Debug Output !-->
-        <?php if ($debug) var_dump($page); ?>
 
-        <!-- Header !-->
-        <?php if ($page['show_header']): ?>
-            <div class="header-wrapper">
-                <?php include ('templates/objects/header.tpl.php'); ?>
-            </div>
-        <?php endif; ?>
-
-        <!-- Content !-->
-        <?php if (isset($page['content']['rendered'])): ?>
-            <div class="content-wrapper">
-                <?php print $page['content']['rendered']; ?>
-            </div>
-        <?php endif; ?>
-
-        <!-- Footer !-->
-        <?php if ($page['show_footer']): ?>
-            <div class="footer-wrapper">
-                <?php include ('templates/objects/footer.tpl.php'); ?>
-            </div>
-        <?php endif; ?>
     </body>
 </html>
--- templates/objects/header.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="header">
-    <h1>Pasirinkite puslapį į kurį norite eiti</h1>
-    <?php foreach ($page['header_links'] as $link): ?>
-        <a href="<?php print $link['url'] ?>"><?php print $link['text'] ?></a>
-    <?php endforeach; ?>
-</div>
