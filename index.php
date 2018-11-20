28c4d81de6db90e6faa5ae171a3d11936db6802f
1995tadas <1995tadas@gmail.com>
2018-11-20 09:35:54 +0200

    PHP class function extend 3

--- index.php
+++ index.php
@@ -4,18 +4,30 @@ class Dacikas {
 
     protected $pask_matavimai;
 
+    /*
+     * isspausdinti $pask_matavimai
+     */
+
     public function gautiNaujusMatavimus() {
-        print "Nežinai, ką matuot";
+        if ($this->pask_matavimai) {
+            print $this->pask_matavimai;
+        } else {
+            print "Nežinai, ką matuot <br>";
+        }
     }
 
 }
 
 class TempDacikas extends Dacikas {
 
-
+    /**
+     * Nustatyti $pask_matavimai ir isspausdinti su parentu
+     * @return type
+     */
     public function gautiNaujusMatavimus() {
-        $this ->pask_matavimai = "Dabar yra " . rand(0, 20) . " C silumos";
-        print $this->pask_matavimai;
+
+        $this->pask_matavimai = "Dabar yra " . rand(0, 20) . " C silumos <br>";
+        parent::gautiNaujusMatavimus();
     }
 
 }
